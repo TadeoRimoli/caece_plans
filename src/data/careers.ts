64f2475190c67bf083349b7c79b29815bcc5ec8e
Tadeo Rimoli <taderimoli@gmail.com>
2025-08-06 11:58:57 -0300

    Add :<Agrega plan de ingeniería 2010>

--- src/data/careers.ts
+++ src/data/careers.ts
@@ -644,5 +644,355 @@ export const careersData: Career[] = [
       status: 0
     }
   ]
-}
+},
+{id: 3,
+  name: "Ingeniería en Sistemas",
+  plan: "10",
+  year: 2010,
+  icon :"💻",
+  subjects: [
+    {
+      code: 7301,
+      name: "Introducción a la Matemática",
+      year: 1,
+      quadrimester: 1,
+      prerequisites: [],
+      status: 0
+    },
+    {
+      code: 8015,
+      name: "Introducción a la Informática",
+      year: 1,
+      quadrimester: 1,
+      prerequisites: [],
+      status: 0
+    },
+    {
+      code: 1344,
+      name: "Química",
+      year: 1,
+      quadrimester: 1,
+      prerequisites: [],
+      status: 0
+    },
+     {
+      code: 2092,
+      name: "Sistemas de Representación",
+      year: 1,
+      quadrimester: 1,
+      prerequisites: [],
+      status: 0
+    },
+     {
+      code: 1618,
+      name: "Cálculo en una Variable",
+      year: 1,
+      quadrimester: 2,
+      prerequisites: [7301],
+      status: 0
+    },
+    {
+      code: 7256,
+      name: "Arquitectura de Computadoras",
+      year: 1,
+      quadrimester: 2,
+      prerequisites: [],
+      status: 0
+    },
+    {
+      code: 7307,
+      name: "Algoritmos y Estructuras de Datos I",
+      year: 1,
+      quadrimester: 2,
+      prerequisites: [8015],
+      status: 0
+    },
+
+    {
+      code: 2076,
+      name: "Aspectos Profesionales de Sistemas",
+      year: 1,
+      quadrimester: 2,
+      prerequisites: [],
+      status: 0
+    },
+    {
+      code: 7014,
+      name: "Álgebra",
+      year: 2,
+      quadrimester: 1,
+      prerequisites: [7301],
+      status: 0
+    },
+    {
+      code: 7309,
+      name: "Algoritmos y Estructuras de Datos II",
+      year: 2,
+      quadrimester: 1,
+      prerequisites: [7307],
+      status: 0
+    },
+    {
+      code: 3527,
+      name: "Física I",
+      year: 2,
+      quadrimester: 1,
+      prerequisites: [1618],
+      status: 0
+    },
+    {
+      code: 3617,
+      name: "Física II",
+      year: 2,
+      quadrimester: 2,
+      prerequisites: [3527],
+      status: 0
+    },
+    {
+      code: 7025,
+      name: "Matemática Discreta",
+      year: 2,
+      quadrimester: 2,
+      prerequisites: [7301],
+      status: 0
+    },
+    {
+      code: 1620,
+      name: "Sistemas Operativos I",
+      year: 2,
+      quadrimester: 2,
+      prerequisites: [7256,7307],
+      status: 0
+    },
+
+    {
+      code: 1623,
+      name: "Cálculo en Varias Variables",
+      year: 2,
+      quadrimester: 2,
+      prerequisites: [1618,7014],
+      status: 0
+    },
+
+    {
+      code: 1001,
+      name: "Inglés Técnico",
+      year: 2,
+      quadrimester: 2,
+      prerequisites: [],
+      status: 0
+    },
+    
+   {
+      code: 7319,
+      name: "Probabilidad y Estadística",
+      year: 3,
+      quadrimester: 1,
+      prerequisites: [1623],
+      status: 0
+    },
+        {
+      code: 1189,
+      name: "Ingeniería de Software I",
+      year: 3,
+      quadrimester: 1,
+      prerequisites: [1001,7307],
+      status: 0
+    },
+    {
+      code: 2077,
+      name: "Bases de Datos I",
+      year: 3,
+      quadrimester: 1,
+      prerequisites: [1620,7014],
+      status: 0
+    },
+    {
+      code: 1343,
+      name: "Economía",
+      year: 3,
+      quadrimester: 1,
+      prerequisites: [1618],
+      status: 0
+    },
+
+        {
+      code: 1190,
+      name: "Ingeniería de Software II",
+      year: 3,
+      quadrimester: 2,
+      prerequisites: [1189,2077],
+      status: 0
+    },
+      {
+      code: 7315,
+      name: "Fundamentos de Comunicación",
+      year: 3,
+      quadrimester: 2,
+      prerequisites: [1623],
+      status: 0
+    },
+       {
+      code: 2079,
+      name: "Sistemas de Información y Organización",
+      year: 3,
+      quadrimester: 2,
+      prerequisites: [],
+      status: 0
+    },
+    {
+      code: 2094,
+      name: "Lenguajes de Programación",
+      year: 3,
+      quadrimester: 2,
+      prerequisites: [7309],
+      status: 0
+    },
+    {
+      code: 1291,
+      name: "Modelos y Simulación",
+      year: 4,
+      quadrimester: 1,
+      prerequisites: [7307,7319],
+      status: 0
+    },
+    {
+      code: 7283,
+      name: "Teleprocesos y Redes I",
+      year: 4,
+      quadrimester: 1,
+      prerequisites: [1620,7315],
+      status: 0
+    },
+     {
+      code: 7329,
+      name: "Lenguajes Formales",
+      year: 4,
+      quadrimester: 1,
+      prerequisites: [7025,7307],
+      status: 0
+    },
+     {
+      code: 1350,
+      name: "Sistemas Inteligentes",
+      year: 4,
+      quadrimester: 1,
+      prerequisites: [7025,7309],
+      status: 0
+    },
+     {
+      code: 1354,
+      name: "GESTION AMBIENTAL ",
+      year: 4,
+      quadrimester: 1,
+      prerequisites: [1344],
+      status: 0
+    },
+    
+    
+    {
+      code: 7292,
+      name: "Teleprocesos y Redes II",
+      year: 4,
+      quadrimester: 2,
+      prerequisites: [7283],
+      status: 0
+    },
+    {
+      code: 1638,
+      name: "Arquitectura Web",
+      year: 4,
+      quadrimester: 2,
+      prerequisites: [1190,7283],
+      status: 0
+    },
+    {
+      code: 2078,
+      name: "Bases de Datos II",
+      year: 4,
+      quadrimester: 2,
+      prerequisites: [2077],
+      status: 0
+    },
+    {
+      code: 2081,
+      name: "Gestión de Proyectos y Calidad",
+      year: 4,
+      quadrimester: 2,
+      prerequisites: [1190],
+      status: 0
+    },
+   
+    {
+      code: 1629,
+      name: "Sistemas Operativos II",
+      year: 5,
+      quadrimester: 1,
+      prerequisites: [1620,7292],
+      status: 0
+    },
+  {
+      code: 2083,
+      name: "Dirección Estratégica",
+      year: 5,
+      quadrimester: 1,
+      prerequisites: [2079],
+      status: 0
+    },
+
+    {
+      code: 2095,
+      name: "Proyecto Final Ingeniería I",
+      year: 5,
+      quadrimester: 1,
+      prerequisites: [2078,2081,7292],
+      extraConditions: "",
+      status: 0
+    },
+    {
+      code: 2085,
+      name: "Inteligencia de Negocios",
+      year: 5,
+      quadrimester: 2,
+      prerequisites: [1350,2078],
+      status: 0
+    },
+     {
+      code: 1635,
+      name: "Arquitectura Avanzada",
+      year: 5,
+      quadrimester: 2,
+      prerequisites: [1620,3617,7292],
+      status: 0
+    },
+    {
+      code: 2088,
+      name: "Auditoría y Seguridad Informática",
+      year: 5,
+      quadrimester: 2,
+      prerequisites: [2081,7283],
+      status: 0
+    },
+   
+  
+    {
+      code: 2091,
+      name: "Práctica Profesional Supervisada",
+      year: 5,
+      quadrimester: 2,
+      prerequisites: [],
+      extraConditions: "28 asignaturas aprobadas",
+      status: 0
+    },
+    {
+      code: 2096,
+      name: "Proyecto Final Ingeniería II",
+      year: 5,
+      quadrimester: 2,
+      prerequisites: [2095],
+      status: 0
+    }
+  ]
+    
+  }
 ];
